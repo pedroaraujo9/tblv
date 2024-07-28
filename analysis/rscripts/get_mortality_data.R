d48c114706aa0a0b519adb51ff6e378de105679b
Pedro Araújo <pedromenezes98@gmail.com>
2024-07-28 10:41:26 +0100

    add new package and data

--- analysis/rscripts/get_mortality_data.R
+++ analysis/rscripts/get_mortality_data.R
@@ -8,22 +8,31 @@ library(yaml)
 cred = yaml.load_file("analysis/rscripts/config.yaml")
 
 # countries names 
-countries = HMDHFDplus::getHMDcountries()
 
 # read life tables for all countries and build a data.frame with relevant info
-life_tables = map_df(countries$CNTRY, ~{
-  readHMDweb(.x, 
-             item = "bltper_5x5", 
-             username = cred$username, 
-             password = cred$password) %>%
-    mutate(country_code = .x)
-}) %>%
-  select(Year, Age, mx, qx, OpenInterval, country_code) %>%
-  left_join(countries %>% select(-link), by = c("country_code" = "CNTRY"))
+download_mortality = function(credentials, age_group_size, cohort_size) {
+  countries = HMDHFDplus::getHMDcountries()
+  life_tables = map_df(countries$CNTRY, ~{
+    readHMDweb(.x, 
+               item = paste0("bltper_", age_group_size, "x", cohort_size), 
+               username = credentials$username, 
+               password = credentials$password) %>%
+      mutate(country_code = .x)
+  }) %>%
+    select(Year, Age, mx, qx, OpenInterval, country_code) %>%
+    left_join(countries %>% select(-link), by = c("country_code" = "CNTRY"))
+  
+  names(life_tables) = names(life_tables) %>% tolower()
+  life_tables$date_extract = Sys.Date()
+  return(life_tables)
+}
 
-names(life_tables) = names(life_tables) %>% tolower()
+## life tables period data
+download_mortality(cred, 1, 1) %>% saveRDS("analysis/data/life_tables_1x1.rds")
+download_mortality(cred, 1, 5) %>% saveRDS("analysis/data/life_tables_1x5.rds")
+download_mortality(cred, 1, 10) %>% saveRDS("analysis/data/life_tables_1x10.rds")
 
-life_tables %>% glimpse()
+download_mortality(cred, 5, 1) %>% saveRDS("analysis/data/life_tables_5x1.rds")
+download_mortality(cred, 5, 5) %>% saveRDS("analysis/data/life_tables_5x5.rds")
+download_mortality(cred, 5, 10) %>% saveRDS("analysis/data/life_tables_5x10.rds")
 
-### this data is available on the tblv package
-saveRDS(life_tables, "analysis/data/life_tables_5x5.rds")
---
+++ packages/btblv/R/btblv-package.R
@@ -0,0 +1,24 @@
+#' The 'btblv' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name btblv-package
+#' @aliases btblv
+#' @useDynLib btblv, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import dplyr
+#' @import tibble
+#' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.6. https://mc-stan.org
+#'
+#' ## usethis namespace: start
+#' @importFrom magrittr %>%
+## usethis namespace: end
+NULL
+NULL
