00e0cf41941dbcfd13ddb9c0f448ff5fbccdaf9b
Pedro Araújo <pedromenezes98@gmail.com>
2024-07-30 00:05:21 +0100

    add files to fit models

--- analysis/rscripts/fit_mortality_data.R
+++ analysis/rscripts/fit_mortality_data.R
@@ -1,58 +1,72 @@
 library(btblv)
+library(dplyr)
+library(stringr)
 
-#### getting terminal parameters ####
+#### parameters from the terminal ####
 args = commandArgs(trailingOnly = T)[-1]
 args_list = list()
 
 for(i in 1:length(args)) {
-  name_value = args[i] %>% str_split("=", simplify = T) %>% as.character()
-  args_list[name_value[1]] = name_value[2] %>% as.numeric()
+  name_value = args[i] %>% stringr::str_split("=", simplify = T) %>% as.character()
+  args_list[name_value[1]] = name_value[2] 
 }
 
+print(args)
 print(args_list)
 
-K = args_list$K
-iter = args_list$iter
-warmup = args_list$warmup
-chains = args_list$chains
-thin = args_list$thin
-precision = args_list$precision
-path_to_save = args_list$path_to_save
+K = args_list$K %>% as.numeric()
+iter = args_list$iter %>% as.numeric()
+warmup = args_list$warmup %>% as.numeric()
+chains = args_list$chains %>% as.numeric()
+thin = args_list$thin %>% as.numeric()
+precision = args_list$precision 
+path_to_save = args_list$path_to_save 
 
+#### check if the model was already fitted ####
 model_name = paste0(
   "btblv-precision=", precision, "-",
   "K=", K, ".rds"
 )
 
-models_saved = scan("analysis/models/models-saved-list.txt")
+print(model_name)
 
-list.files("analysis")
-#### data ####
-data("lf")
-
-lf = lf %>%
-  filter(year %in% seq(1950, 2015, 5)) %>%
-  filter(!(country %in% c("East Germany", "West Germany", "New Zealand Maori",
-                          "New Zealand Non-Maori", "England and Wales (Total Population)",
-                          "England and Wales (Civilian Population)",
-                          "Scotland", "Northern Ireland", "Wales")))
-
-data = btblv::create_btblv_data(df = lf,
-                                resp_col_name = "mx",
-                                item_col_name = "age",
-                                group_col_name = "country",
-                                time_col_name = "year")
-
-fit = btblv_fit(data,
-                precision = precision,
-                K = K,
-                iter = iter,
-                warmup = warmup,
-                thin = thin,
-                chains = chains,
-                cores = chains,
-                seed = 1)
-
-saveRDS(fit, "models/")
-
-
+#### fit the model in case the model has not been fitted ####
+path_to_save = "analysis/models/"
+models_saved = readLines(paste0(path_to_save, "models-saved-list.txt"))
+models_saved
+if(!(model_name %in% models_saved)) {
+  #### data ####
+  data("lf")
+  
+  lf = lf %>%
+    filter(year %in% seq(1950, 2015, 5)) %>%
+    filter(!(country %in% c("East Germany", "West Germany", "New Zealand Maori",
+                            "New Zealand Non-Maori", "England and Wales (Total Population)",
+                            "England and Wales (Civilian Population)",
+                            "Scotland", "Northern Ireland", "Wales")))
+  
+  data = btblv::create_btblv_data(df = lf,
+                                  resp_col_name = "mx",
+                                  item_col_name = "age",
+                                  group_col_name = "country",
+                                  time_col_name = "year")
+  
+  fit = btblv_fit(data,
+                  precision = precision,
+                  K = K,
+                  iter = iter,
+                  warmup = warmup,
+                  thin = thin,
+                  chains = chains,
+                  cores = chains,
+                  seed = 1)
+  
+  saveRDS(fit, paste0(path_to_save, model_name))
+  
+  # updating list with models saved
+  c(model_name, models_saved) %>%
+  writeLines(paste0(path_to_save, "models-saved-list.txt"))
+  
+}else{
+  print("Fit already exists")
+}
