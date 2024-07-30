0dd0efb4ff1ca517b133878ec79b86c641483bd4
Pedro Araújo <pedromenezes98@gmail.com>
2024-07-30 16:44:00 +0100

    code to fit model locally and in a server

---
+++ analysis/bashscripts/cluster_fit_mort_specific_prec.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -l
+
+# Set the number of nodes
+
+#SBATCH -N 1
+
+# Set the number of tasks/cores per node required 
+#SBATCH -n 35
+
+# Set the walltime of the job to 1 hour (format is hh:mm:ss)
+#SBATCH -t 100:00:00
+
+# E-mail on begin (b), abort (a) and end (e) of job
+#SBATCH --mail-type=ALL
+
+# E-mail address of recipient
+#SBATCH --mail-user=pedro.menezesdearaujo@ucdconnect.ie
+
+# Specifies the jobname
+#SBATCH --job-name=single-precision
+
+iter=10000;warmup=5000;thin=10; chains=3;
+
+path="analysis/models"
+gdrive=TRUE
+gdrive_folder_id="1qlT8pJsHhzQF8-XCa7bS-f5fmEwcEIcB"
+model="specific"
+
+module load R
+module load python
+
+for K in $(seq 1 $max_K)
+  do 
+  Rscript rscripts/fit_mortality_data.R --args K=$K iter=$iter warmup=$warmup thin=$thin chains=$chains precision=$model path_to_save=$path save_gdrive=$gdrive gdrive_folder_id=$gdrive_folder_id & 
+  done
+
+wait;
+
+
---
+++ analysis/rscripts/download_models_gdrive.R
@@ -0,0 +1,15 @@
+library(googledrive)
+library(tidyverse)
+
+drive_auth_configure(path = "credentials.json")
+
+folder = "analysis/models"
+folder_id = "1qlT8pJsHhzQF8-XCa7bS-f5fmEwcEIcB"
+models = drive_ls(path = as_id(folder_id))
+print(models)
+
+for(i in 1:nrow(models)) {
+  drive_download(as_id(models$id[i]),
+                 path = file.path(folder, models$name[i]),
+                 overwrite = TRUE)
+}
