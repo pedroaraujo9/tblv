3fc66b900e31b079af8e8926e6a5333445b6f518
Pedro Araújo <pedromenezes98@gmail.com>
2024-08-05 00:06:52 +0100

    fix names and remote scripts

--- analysis/bashscripts/remote_fit_mort_specific.sh
+++ analysis/bashscripts/remote_fit_mort_specific.sh
@@ -31,6 +31,8 @@ data_path="analysis/data/data_model.rds"
 local_save_path="analysis/models_test"
 gdrive_folder_id="1LvmQrUG3P424ZsEVi_LuWfgUHoLmf-LH"
 
+module load R
+
 for K in $(seq 1 10)
   do 
   Rscript analysis/rscripts/fit_mortality_data.R --args K=$K iter=$iter warmup=$warmup thin=$thin chains=$chains precision=$precision config_path=$config_path model_name_pattern=$model_name_pattern save_gdrive=$save_gdrive data_path=$data_path local_save_path=$local_save_path gdrive_folder_id=$gdrive_folder_id &  
--- analysis/bashscripts/remote_fit_sim_data.sh
+++ analysis/bashscripts/remote_fit_sim_data.sh
@@ -33,6 +33,8 @@ data_path=$2
 local_save_path="analysis/models/simulation-study"
 gdrive_folder_id="1rNyxQWOLd9MjxXkR2vWnin-l2Xvuu65j"
 
+module load R
+
 for K in $(seq 1 6); do 
   Rscript analysis/rscripts/fit_mortality_data.R --args K=$K iter=$iter warmup=$warmup thin=$thin chains=$chains precision=$precision config_path=$config_path model_name_pattern=$model_name_pattern save_gdrive=$save_gdrive data_path=$data_path local_save_path=$local_save_path gdrive_folder_id=$gdrive_folder_id & 
 done
