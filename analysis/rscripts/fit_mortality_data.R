d7c2aac92e3df9f1e8430a608fda90e45ce9fc97
Pedro Araújo <pedromenezes98@gmail.com>
2024-08-04 17:28:07 +0100

    change bash for local fit

--- analysis/rscripts/fit_mortality_data.R
+++ analysis/rscripts/fit_mortality_data.R
@@ -3,6 +3,9 @@ library(dplyr)
 library(stringr)
 library(googledrive)
 
+#### config ####
+config = yaml::yaml.load_file("config.yaml")
+
 #### parameters from the terminal ####
 args = commandArgs(trailingOnly = T)[-1]
 args_list = list()
@@ -15,16 +18,29 @@ for(i in 1:length(args)) {
 print(args)
 print(args_list)
 
+# data
+data_path = args_list$data_path
+
+# model inputs 
 K = args_list$K %>% as.numeric()
 iter = args_list$iter %>% as.numeric()
 warmup = args_list$warmup %>% as.numeric()
 chains = args_list$chains %>% as.numeric()
 thin = args_list$thin %>% as.numeric()
-precision = args_list$precision 
+precision = args_list$precision
 
+# Google drive config/saving config
 path_to_save = args_list$path_to_save 
 save_gdrive = args_list$save_gdrive %>% as.logical()
-gdrive_folder_id = args_list$gdrive_folder_id
+
+if(save_gdrive == TRUE) {
+  gdrive_folder_id = config$gdrive$model_folder_id
+}else{
+  gdrive_folder_id = NULL
+}
+
+#### read data ####
+mortality_data = readRDS(data_path)
 
 #### check if the model was already fitted ####
 model_name = paste0(
@@ -39,19 +55,17 @@ models_saved = readLines(paste0(path_to_save, "/models-saved-list.txt"))
 print(models_saved)
 
 if(!(model_name %in% models_saved)) {
-  #### data ####
-  lf = readRDS("analysis/data/data_model.rds")
   
   data = btblv::create_btblv_data(
-    df = lf,
+    df = mortality_data,
     resp_col_name = "mx",
     item_col_name = "age",
     group_col_name = "country",
     time_col_name = "year"
   )
   
-  fit = fit_btblv(
-    data,
+  fit = btblv::fit_btblv(
+    btblv_data = data,
     precision = precision,
     K = K,
     iter = iter,
@@ -62,25 +76,25 @@ if(!(model_name %in% models_saved)) {
     seed = 1
   )
   
-  saveRDS(fit, paste0(path_to_save, "/",model_name))
+  #saveRDS(fit, paste0(path_to_save, "/",model_name))
   
-  if(save_gdrive == TRUE) {
-    drive_deauth()
-    drive_auth_configure(path = "credentials.json")
-    drive_auth()
+  #if(save_gdrive == TRUE) {
+  #  googledrive::drive_deauth()
+  #  googledrive::drive_auth_configure(path = config$gdrive$auth_credentials)
+  #  googledrive::drive_auth()
     
-    drive_upload(media = paste0(path_to_save, "/",model_name), 
-                 path = as_id(gdrive_folder_id),
-                 overwrite = TRUE)
-    
-    file.remove(paste0(path_to_save, "/",model_name))
-  }
+  #  googledrive::drive_upload(media = paste0(path_to_save, "/",model_name), 
+  #                            path = as_id(gdrive_folder_id),
+  #                            overwrite = TRUE)
+  #  
+  #  file.remove(paste0(path_to_save, "/",model_name))
+  #}
   
   # updating list with models saved
-  c(model_name, models_saved) %>%
-    writeLines(paste0(path_to_save, "/models-saved-list.txt"))
+  #c(model_name, models_saved) %>%
+  #  writeLines(paste0(path_to_save, "/models-saved-list.txt"))
   
 }else{
-  print("Fit already exists")
+  #print("Fit already exists")
 }
 
