3c8dff36df938ccaa8c9da9870052c8c9f6380f7
Pedro Araújo <pedromenezes98@gmail.com>
2024-08-04 17:12:52 +0100

    add config.yaml file template

--- analysis/rscripts/download_models_gdrive.R
+++ analysis/rscripts/download_models_gdrive.R
@@ -2,12 +2,12 @@ library(googledrive)
 library(tidyverse)
 library(yaml)
 
-config = yaml.load_file("config.yaml")
-drive_auth_configure(path = "credentials.json")
+config = yaml::yaml.load_file("config.yaml")
+googledrive::drive_auth_configure(path = config$gdrive$auth_credentials)
 
 folder = "analysis/models"
-gdrive_folder_id = config$gdrive_folder_id
-models = drive_ls(path = as_id(gdrive_folder_id))
+model_folder_id = config$gdrive$model_folder_id
+models = drive_ls(path = as_id(model_folder_id))
 print(models)
 
 for(i in 1:nrow(models)) {
