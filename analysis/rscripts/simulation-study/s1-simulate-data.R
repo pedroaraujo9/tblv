5614433cc230bb0fa93e439e07b2c06d289dabb9
Pedro Araújo <pedromenezes98@gmail.com>
2026-02-04 17:47:25 +0000

    create new simulated data

--- analysis/rscripts/simulation-study/s1-simulate-data.R
+++ analysis/rscripts/simulation-study/s1-simulate-data.R
@@ -3,11 +3,11 @@ library(btblv)
 for(K in c(2, 4)) {
   
   fit = readRDS(
-    paste0("analysis/models/btblv-precision=single-K=", K, ".rds")
+    paste0("analysis/models/btblv-qx-incomplete-precision=single-K=", K, ".rds")
   )
   
-  post = fit |> btblv::extract_posterior()
-  sim_data = post |> btblv::simulate_data(replicates = 30, seed = 1)
+  post = fit$btblv_fit |> btblv::extract_posterior()
+  sim_data = post |> btblv::simulate_data(replicates = 50, seed = 1)
   
   saveRDS(sim_data, paste0("analysis/data/sim_data_", K, ".rds"))
   
@@ -18,7 +18,7 @@ for(K in c(2, 4)) {
     )
     
     sim_data$sim_data_list[[i]] |> 
-      btblv::create_btblv_data("mx", "age", "country", "year") |>
+      btblv::create_btblv_data("qx", "age", "country", "year") |>
       saveRDS(file_name)
   }  
 }
